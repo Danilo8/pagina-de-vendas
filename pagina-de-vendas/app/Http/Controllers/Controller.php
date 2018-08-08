9bb46ad7dc8f96f85410e4b7fe5dc50bdf34cffd
Danilo Gonçalves dos Santos <danilodesenvolvedorweb@hotmail.com>
2018-08-08 14:17:49 -0300

    Iniciando o projeto

---
+++ pagina-de-vendas/app/Http/Controllers/Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Função que retorna a view INDEX
+     */
+    public function index()
+    {
+        return view('index');
+    }
+}
